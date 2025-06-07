a06cbfdc7ec45b4bfdc7f2ae519d1131e8b472f2
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-06-07 20:12:42 +0530

    almost done homepage

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,5 @@
 let main = document.querySelector(".main");
 let cursor = document.querySelector(".cursor");
-// let imageDiv = document.querySelector(".image");
 
 main.addEventListener("mousemove",function(dets){
     gsap.to(cursor,{
@@ -9,20 +8,3 @@ main.addEventListener("mousemove",function(dets){
         duration: .5,
     })
 })
-
-// imageDiv.addEventListener("mouseenter",function(){
-//     cursor.innerHTML = "View More"
-//     gsap.to(cursor,{
-//         scale: 10,
-//         opacity: .7,
-//         backgroundColor: "rgb(207, 104, 124)",
-//     })
-// })
-// imageDiv.addEventListener("mouseleave",function(){
-//     cursor.innerHTML = ""
-//     gsap.to(cursor,{
-//         scale: 1,
-//         opacity: 1,
-//         backgroundColor: "white",
-//     })
-// })

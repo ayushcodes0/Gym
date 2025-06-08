92350a55efa5d078ace35875a1bcbb57a9579deb
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-06-08 13:59:35 +0530

    Completed responsiveness

--- javascript/script.js
+++ javascript/script.js
@@ -8,3 +8,17 @@ main.addEventListener("mousemove",function(dets){
         duration: .5,
     })
 })
+
+const menu_content = document.getElementById("menu-content");
+const menu_button = document.getElementById("nav-right");
+const cross_button = document.getElementById("cross-icon");
+
+menu_button.addEventListener("click", function(){
+    menu_content.style.top = "0";
+    menu_content.style.opacity = "1";
+})
+
+cross_button.addEventListener("click", function(){
+    menu_content.style.top = "100vh";
+    menu_content.style.opacity = "0";
+})
